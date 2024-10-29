44cc30dcb3932fa9ca28964cdb64d5e1e0e03eea
ywilfredo <74474078+ywilfredo@users.noreply.github.com>
2024-10-28 20:27:27 -0400

    added interface and implementation for Country model

---
+++ Repository/Implementations/CountryRepository.cs
@@ -0,0 +1,65 @@
+﻿using Sales.Data;
+using Sales.Models;
+using Sales.Repository.Interfaces;
+
+namespace Sales.Repository.Implementations
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly SalesDbContext _context;
+        public CountryRepository( SalesDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Country> AllCountries => _context.Countries.ToList();
+
+        public void Create(Country country)
+        {
+            try
+            {
+                _context.Countries.Add(country);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Delete(Country country)
+        {
+            try
+            {
+                _context.Countries.Remove(country);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Country? GetById(int id)
+        {
+            Country? country = _context.Countries
+                .FirstOrDefault(c => c.Id == id);
+            return country;
+        }
+
+        public void Update(Country country)
+        {
+            try
+            {
+                _context.Countries.Update(country);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Repository/Interfaces/ICountryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Sales.Models;
+
+namespace Sales.Repository.Interfaces
+{
+    public interface ICountryRepository
+    {
+        IEnumerable<Country> AllCountries { get; }
+        Country? GetById(int id);
+
+        void Create(Country country);
+
+        void Update(Country country);
+
+        void Delete(Country country);
+    }
+}
